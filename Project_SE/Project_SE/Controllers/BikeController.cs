b5d6086f420ea940cd8119993036440250ce98d2
Costinelos <148136013+Costinelos@users.noreply.github.com>
2025-04-14 14:39:25 +0300

    Frontend beta (#19)
    -

--- Project_SE/Project_SE/Controllers/BikeController.cs
+++ Project_SE/Project_SE/Controllers/BikeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project_SE.Models;
+using System.Collections.Generic;
 
 namespace Project_SE.Controllers
 {
@@ -6,7 +8,14 @@ namespace Project_SE.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // Date hardcodate
+            var bikes = new List<Bike>
+            {
+                new Bike { Id = 1, Model = "Rockrider ST100", Type = "MTB", Price = 80 },
+                new Bike { Id = 2, Model = "Triban RC100", Type = "Șosea", Price = 100 }
+            };
+
+            return View(bikes);
         }
     }
 }
