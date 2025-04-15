a57bdca58e75250067c05c557814ecc3d5405a12
costi <costi@LAPTOP-EKHLUO7S>
2025-04-15 10:43:23 +0300

    -

--- Project_SE/Project_SE/Models/Bike.cs
+++ Project_SE/Project_SE/Models/Bike.cs
@@ -3,9 +3,9 @@
     public class Bike
     {
         public int Id { get; set; }
-        public string Model { get; set; } = string.Empty; // Evită warning-uri
+        public string Model { get; set; } = string.Empty; 
         public string Type { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public string Status { get; set; } = "Disponibil"; // Valoare implicită
+        public string Status { get; set; } = "Disponibil"; 
     }
 }
