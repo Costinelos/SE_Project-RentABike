9f19242c853e619342164c9e45198e97cf034ce6
Costinelos <148136013+Costinelos@users.noreply.github.com>
2025-04-27 20:47:58 +0300

    Back end login sys (#23)
    * Frontend beta
    
    * Fix minor mistake
    
    * some changes
    
    * -
    
    * Login & register system
    
    * Update Bike.cs
    
    * Update Login.cshtml
    
    ---------
    
    Co-authored-by: costi <costi@LAPTOP-EKHLUO7S>

---
+++ Project_SE/Project_SE/Data/ApplicationDBContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project_SE.Models;
+
+namespace Project_SE.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bike> Bikes { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configure relationships
+            builder.Entity<Reservation>()
+                .HasOne(r => r.User)
+                .WithMany()
+                .HasForeignKey(r => r.UserId);
+
+            builder.Entity<Reservation>()
+                .HasOne(r => r.Bike)
+                .WithMany()
+                .HasForeignKey(r => r.BikeId);
+
+            builder.Entity<Notification>()
+                .HasOne(n => n.User)
+                .WithMany()
+                .HasForeignKey(n => n.UserId);
+        }
+    }
+}
---
+++ Project_SE/Project_SE/Models/LoginViewModels.cs
@@ -0,0 +1,20 @@
+﻿// LoginViewModel.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_SE.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email-ul este obligatoriu")]
+        [EmailAddress(ErrorMessage = "Email invalid")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Parola este obligatorie")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Ține-mă minte")]
+        public bool RememberMe { get; set; }
+    }
+}
+
