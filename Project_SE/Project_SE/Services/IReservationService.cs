c6e016e14e07909009710c322b5119dcb66f1efb
costi <costi@LAPTOP-EKHLUO7S>
2025-05-06 00:18:27 +0300

    solved problems

---
+++ Project_SE/Project_SE/Services/IReservationService.cs
@@ -0,0 +1,16 @@
+﻿// Interfaces/IReservationService.cs
+using Project_SE.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Project_SE.Interfaces
+{
+    public interface IReservationService
+    {
+        Task<IEnumerable<Reservation>> GetUserReservationsAsync(string userId);
+        Task<Reservation> GetReservationByIdAsync(int id);
+        Task<bool> CreateReservationAsync(Reservation reservation);
+        Task<bool> UpdateReservationStatusAsync(int id, string status);
+        Task<IEnumerable<Reservation>> GetAllReservationsAsync();
+    }
+}
